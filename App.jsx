1561e2efeefb36f96499513c91ae98ca9230b965
prabhjot-2 <63330693+prabhjot-2@users.noreply.github.com>
2023-03-08 16:51:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const App=()=>{
+
+
+    const incNum=()=>{
+        setNum(num+1);
+    };
+    const decNum=()=>{
+        if(num>0){
+            setNum(num-1);
+        }
+        else{
+            alert("sorry zero limit reached");
+            setNum(0);
+        }
+    };
+    const[num, setNum]=useState(0);
+
+    return (
+        <>
+            <div className="main_div">
+                <div className="center_div">
+                    <h1>
+                        {num}
+                    </h1>
+                    <div className="btn_div">
+                        <button onClick={incNum}>
+                            Increm
+                        </button>
+                        <button onClick={decNum}>
+                            Decrem
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
+
